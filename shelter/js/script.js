cfb0988eab7fd474934653f7dabc8a3dcc94c476
Ivan Hryhoriev <ivan_gricyan82@ukr.net>
2022-04-11 23:51:30 +0200

    feat: add up to top button

--- shelter/js/script.js
+++ shelter/js/script.js
@@ -1,6 +1,22 @@
 const logo = document.querySelector('.logo');
+const upBtn = document.querySelector('.up-btn');
 
 logo.addEventListener('click', () => {
   location.href = "index.html";
 
 })
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= screen.height) {
+    upBtn.style.transform = 'translateY(0)';
+  } else {
+    upBtn.style.transform = 'translateY(200%)';
+  }
+});
+
+upBtn.addEventListener("click", toTopScroll);
+
+function toTopScroll() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
