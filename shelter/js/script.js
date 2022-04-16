83327c83b0707205e5e9d0727eea83cfce78a3a5
Ivan Hryhoriev <ivan_gricyan82@ukr.net>
2022-04-16 23:27:42 +0200

    feat: add burger menu

--- shelter/js/script.js
+++ shelter/js/script.js
@@ -20,3 +20,28 @@ function toTopScroll() {
   document.body.scrollTop = 0
   document.documentElement.scrollTop = 0
 }
+
+/* Burger menu */
+
+const hamburger = document.querySelector('.burger')
+const nav = document.querySelector('.nav')
+const navLinks = document.querySelectorAll('.nav__link')
+
+
+function toggleMenu() {
+  hamburger.classList.toggle('nav__link_active')
+  nav.classList.toggle('nav__link_active')
+  navWrapper.classList.toggle('nav__link_active')
+}
+
+hamburger.addEventListener('click', toggleMenu)
+nav.addEventListener('click', closeMenu)
+navLinks.forEach((el) => el.addEventListener('click', closeMenu))
+
+function closeMenu(event) {
+  if (event.target.classList.contains('nav__link')) {
+    hamburger.classList.remove('nav__link_active')
+    nav.classList.remove('nav__link_active')
+    navWrapper.classList.remove('nav__link_active')
+  }
+}
